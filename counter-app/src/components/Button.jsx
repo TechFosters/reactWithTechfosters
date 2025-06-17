b80e0914db4ba7dbede291f175c6f6f17cc1029c
TechFosters <akkihere8@gmail.com>
2025-06-18 02:05:41 +0530

    introducing state and hooks

--- counter-app/src/components/Button.jsx
+++ counter-app/src/components/Button.jsx
@@ -1,5 +1,5 @@
 export const Button = (obj) =>{
-    console.log(obj)  //props[properties]: "In React, props are immutable objects passed from parent to child components to make components dynamic and reusable."
+    //console.log(obj)  //props[properties]: "In React, props are immutable objects passed from parent to child components to make components dynamic and reusable."
     return <button onClick= {obj.fn} style = {{color: "#fff", backgroundColor: obj.bgColor, margin: '5px', fontSize: '25px', borderRadius: '5px'}}>{obj.value}</button>
 }
 
--- counter-app/src/pages/CounterPage.jsx
+++ counter-app/src/pages/CounterPage.jsx
@@ -1,17 +1,24 @@
+import { useState } from "react"
 import { Button } from "../components/Button"
 import { Message } from "../components/Message"
 
 export const CounterPage = () =>{
-
-    let counter = 0
+let [counter, setCounter]  = useState(0) //creating the state
+    //let counter = 0
     const plus = () =>{
-        counter ++
+        //counter ++
+        setCounter(counter = counter + 1) // updating the state
         console.log(counter)
     }
 
     const minus = () =>{
-        counter --
+        //counter --
+        setCounter(counter = counter - 1) //updating the state
         console.log(counter)
+    }
+
+    const reset = () =>{
+        setCounter(0)
     }
         return (<div>
         <Message msg = "Counter App" msgColor = "blue"/> 
@@ -19,6 +26,7 @@ export const CounterPage = () =>{
         <div style={{display:"flex", alignItems: "center"}}>
         <Button value = "+" bgColor = "red" fn = {plus} />  
         <Button value = "-" bgColor = "green" fn ={minus }/>
+        <Button value = "Reset" bgColor= "gray" fn ={reset} />
         </div>
     </div>)
 }
