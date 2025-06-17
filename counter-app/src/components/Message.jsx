10c0eb71755f4f31797e9be32fa44b7f5edd8175
TechFosters <akkihere8@gmail.com>
2025-06-18 00:16:23 +0530

    working with message component

--- counter-app/src/components/Message.jsx
+++ counter-app/src/components/Message.jsx
@@ -1,3 +1,7 @@
-export const Message = () =>{
-    return <h1>Counter App</h1>
+// export const Message = (props) =>{
+//     return <h1 style={{color: props.color}}>{props.msg}</h1>
+// }
+
+export const Message = ({msg, msgColor}) =>{
+    return <h1 style ={{color: msgColor}}> {msg} </h1>
 }
--- counter-app/src/pages/CounterPage.jsx
+++ counter-app/src/pages/CounterPage.jsx
@@ -2,8 +2,10 @@ import { Button } from "../components/Button"
 import { Message } from "../components/Message"
 
 export const CounterPage = () =>{
+    let counts = 0;
     return (<div>
-        <Message/>
+        <Message msg = "Counter App" msgColor = "blue"/>
+        <Message msg = "Count is " msgColor= "orange"/>
         <div style={{display:"flex", alignItems: "center"}}>
         <Button value = "+" bgColor = "red"/>
         <Button value = "-" bgColor = "green"/>
