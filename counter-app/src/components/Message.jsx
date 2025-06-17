ffae666e3418544b52da14ae8b4a0385124bc005
TechFosters <akkihere8@gmail.com>
2025-06-18 01:36:48 +0530

    two way communication

--- counter-app/src/components/Message.jsx
+++ counter-app/src/components/Message.jsx
@@ -2,6 +2,6 @@
 //     return <h1 style={{color: props.color}}>{props.msg}</h1>
 // }
 
-export const Message = ({msg, msgColor}) =>{
-    return <h1 style ={{color: msgColor}}> {msg} </h1>
+export const Message = ({msg, msgColor, value}) =>{
+    return <h1 style ={{color: msgColor}}> {msg} {value} </h1>
 }
--- counter-app/src/pages/CounterPage.jsx
+++ counter-app/src/pages/CounterPage.jsx
@@ -2,13 +2,23 @@ import { Button } from "../components/Button"
 import { Message } from "../components/Message"
 
 export const CounterPage = () =>{
-    let counts = 0;
-    return (<div>
-        <Message msg = "Counter App" msgColor = "blue"/>
-        <Message msg = "Count is " msgColor= "orange"/>
+
+    let counter = 0
+    const plus = () =>{
+        counter ++
+        console.log(counter)
+    }
+
+    const minus = () =>{
+        counter --
+        console.log(counter)
+    }
+        return (<div>
+        <Message msg = "Counter App" msgColor = "blue"/> 
+        <Message msg = "Count is " msgColor= "orange" value = {counter}/>
         <div style={{display:"flex", alignItems: "center"}}>
-        <Button value = "+" bgColor = "red"/>
-        <Button value = "-" bgColor = "green"/>
+        <Button value = "+" bgColor = "red" fn = {plus} />  
+        <Button value = "-" bgColor = "green" fn ={minus }/>
         </div>
     </div>)
 }
